203a63f6615ec79491ffc037ed50015d2a913827
Vincent Behar <vincent.behar@dailymotion.com>
2020-05-07 18:26:28 +0200

    debug

--- .github/actions/autotag/entrypoint.sh
+++ .github/actions/autotag/entrypoint.sh
@@ -12,4 +12,6 @@ fi
 
 version=$(/autotag --branch=${INPUT_BRANCH} --repo=${INPUT_REPO} --pre-release-name=${INPUT_PRE_RELEASE_NAME} --pre-release-timestamp=${INPUT_PRE_RELEASE_TIMESTAMP} ${DRY_RUN})
 
+echo "version $version"
+
 echo "::set-output name=version::$version"
