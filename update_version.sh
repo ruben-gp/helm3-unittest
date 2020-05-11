642a9e3f93696ab79f49494875b96f66002e92c2
Vincent Behar <vincent.behar@dailymotion.com>
2020-05-11 07:17:26 +0200

    fix version
    can't use `{{.Version}}` from a global hook...

--- update_version.sh
+++ update_version.sh
@@ -2,5 +2,6 @@
 
 # script used by goreleaser - see .goreleaser.yml
 
+version="$(git describe --tags --exact-match 2>/dev/null)"
 cp plugin.tmpl plugin.yaml
-echo "version: $1" >> plugin.yaml
+echo "version: ${version:1}" >> plugin.yaml
