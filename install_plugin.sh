d7b014af777d510f73732d83f44e20f833f0711b
Ruben Gonzalez del Pozo <ruben.gonzalez.del.pozo@gmail.com>
2024-07-09 14:05:43 +0200

    feat: update project dependencies
    DESCRIPTION:
    Updated following project dependencies:
     - go.mod: including indirect dependencies:
       + NEW: github.com/bradleyjkemp/cupaloy/v2 -> v2.8.0
       + github.com/fatih/color -> v1.17.0
       + github.com/mitchellh/mapstructure -> v1.5.0
       + github.com/spf13/cobra -> v1.8.0
       + github.com/stretchr/testify -> v1.9.0
       + NEW: gopkg.in/yaml.v3 -> v3.0.1
       + helm.sh/helm/v3 -> v3.13.0
    
     - .github/workflows:
       + actions/checkout -> v4
       + actions/setup-go -> v5
       + go-version -> 1.22
       + goreleaser/goreleaser-action -> v6
       + fregante/setup-git-user -> v2
       + jenkins-x-plugins/jx-release-version -> v2.7.3
    
    Updated install_plugin.sh, update_version.sh and plugin.yaml, as well
    as .goreleaser.yml file.

--- install_plugin.sh
+++ install_plugin.sh
@@ -2,20 +2,32 @@
 
 # borrowed from https://github.com/helm/helm-2to3/blob/master/scripts/install_plugin.sh
 
+PROJECT_NAME="helm3-unittest"
+PROJECT_GH="ruben-gp/${PROJECT_NAME}"
+HELM_PLUGIN_PATH="${HELM_PLUGIN_DIR}"
+
+# Convert the HELM_PLUGIN_PATH to unix if cygpath is
+# available. This is the case when using MSYS2 or Cygwin
+# on Windows where helm returns a Windows path but we
+# need a Unix path
+if type cygpath >/dev/null 2>&1; then
+  echo Use Sygpath
+  HELM_PLUGIN_PATH=$(cygpath -u "${HELM_PLUGIN_PATH}")
+fi
+
 if [ -n "${HELM_LINTER_PLUGIN_NO_INSTALL_HOOK}" ]; then
     echo "Development mode: not downloading versioned release."
     exit 0
 fi
 
 version="$(git describe --tags --exact-match 2>/dev/null)"
-echo "Downloading and installing helm3-unittest ${version} ..."
+echo "Downloading and installing ${PROJECT_NAME} fork ${version} ..."
 
-url="https://github.com/ruben-gp/helm3-unittest/"
-url+="releases/download/${version}/helm3-unittest_${version:1}_linux_amd64.tar.gz"
+url="https://github.com/${PROJECT_GH}/"
+url+="releases/download/${version}/${PROJECT_NAME}_${version#v}_linux_amd64.tar.gz"
 
 echo "$url"
 
-mkdir -p "bin"
 mkdir -p "releases/${version}"
 
 # Download with curl if possible.
@@ -25,7 +37,8 @@ if [ -x "$(which curl 2>/dev/null)" ]; then
 else
     wget -q "${url}" -O "releases/${version}.tar.gz"
 fi
-tar xzf "releases/${version}.tar.gz" -C "releases/${version}"
-mv "releases/${version}/helm3-unittest" "bin/unittest"
-mv "releases/${version}/helm-plugin.yaml" plugin.yaml
-mv "releases/${version}/README.md" .
+
+echo "Preparing to install into ${HELM_PLUGIN_PATH}"
+tar xzf "releases/${version}.tar.gz" -C "${HELM_PLUGIN_PATH}"
+rm -rf "releases"
+echo "${PROJECT_NAME} installed into ${HELM_PLUGIN_PATH}"
--- update_version.sh
+++ update_version.sh
@@ -3,5 +3,4 @@
 # script used by goreleaser - see .goreleaser.yml
 
 version="$(git describe --tags --exact-match 2>/dev/null)"
-cp plugin.yaml helm-plugin.yaml
-echo "version: ${version:1}" >> helm-plugin.yaml
+echo "version: ${version:1}" >> plugin.yaml
